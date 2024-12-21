942d75de69838cb070829f9e4092d1af14e1d884
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2024-12-20 22:06:52 -0300

    feet: Gerar chave publica e privada do Tokem JWF.

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,18 +1,35 @@
 package com.example.springsecurity.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import net.jcip.annotations.Immutable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-@Bean
+    @Value("$(jwt.public.key)")
+    private RSAPublicKey publickey;
+    @Value("$(jwt.private.key)")
+    private RSAPrivateKey privatekey;
+    @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     http
             .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
@@ -21,6 +38,16 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+}
 
+@Bean
+    public JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withPublicKey(publickey).build();
+}
+@Bean
+    public JwtEncoder jwtEncoder(){
+    JWK jwk = new RSAKey.Builder(this.publickey).privateKey(privatekey).build();
+    var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwks);
 }
 }
